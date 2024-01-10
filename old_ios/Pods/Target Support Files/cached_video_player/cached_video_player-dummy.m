a899acf84fbdee6191ca2485304f692b0ad71dd2
MOHIT CHOUHAN <mchouhan@eiadev.com>
2024-01-10 18:45:48 +0530

    work of file download issue on ios and audio player issue on ios

--- old_ios/Pods/Target Support Files/cached_video_player/cached_video_player-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_cached_video_player : NSObject
-@end
-@implementation PodsDummy_cached_video_player
-@end
--- old_ios/Pods/Target Support Files/photo_gallery/photo_gallery-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_photo_gallery : NSObject
-@end
-@implementation PodsDummy_photo_gallery
-@end
